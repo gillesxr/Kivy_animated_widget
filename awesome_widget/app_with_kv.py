e5e7456a55a64a8d9382a9d88eb2c865c35513a6
gillesxr <gillesxr@tutanota.com>
2024-10-27 18:32:36 +0100

    kv file and root widget creation

--- awesome_widget/app_with_kv.py
+++ awesome_widget/app_with_kv.py
@@ -1,6 +1,10 @@
 import kivy
 kivy.require('2.3.0')
 from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
+
+class RootWidget(AnchorLayout):
+    pass
 
 class MyApp(App):
     pass
