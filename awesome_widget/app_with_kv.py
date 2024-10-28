f1cbef50c9dde101ddd946c71476fb0bdface0d5
gillesxr <gillesxr@tutanota.com>
2024-10-28 11:49:44 +0100

    Make the widget resizable

--- awesome_widget/app_with_kv.py
+++ awesome_widget/app_with_kv.py
@@ -4,15 +4,23 @@ from kivy.animation import Animation
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.widget import Widget
+from kivy.core.window import Window
 
 class WaitBar(Widget):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.pos = (10, 10)
-        animation = Animation(pos=(656, 10), t='in_out_cubic') + Animation(pos=(10, 10), t='in_out_cubic')
-        animation.repeat = True
-        animation.start(self)
+        self.animation = Animation(pos=((Winwow.width * 0.9) - 64, 10), t='in_out_cubic') + Animation(pos=(10, 10), t='in_out_cubic')
+        self.animation.repeat = True
+        self.animation.start(self)
+        Window.bind(on_resize=self.on_window_resize)
+
+    def on_window_resize(self, window, width, height):
+        self.animation.cancel(self)
+        self.animation = Animation(pos=((width * 0.9) - 64 , 10), t='in_out_cubic') + Animation(pos=(10, 10), t='in_out_cubic')
+        self.animation.repeat = True
+        self.animation.start(self)
 
 class RootWidget(AnchorLayout):
     pass
