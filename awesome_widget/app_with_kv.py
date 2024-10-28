9efe64aba08a765c360c79562f86400f47375aeb
gillesxr <gillesxr@tutanota.com>
2024-10-28 15:04:51 +0100

    Code refactoring
    - Create set_animation function in WaitBar class.
    - Create start_animation function in WaitBar class.
    - Add on_open property in MyPopup class.
    - Add docstring to explain the WaitBar widget end position.
    - Add type hint.

--- awesome_widget/app_with_kv.py
+++ awesome_widget/app_with_kv.py
@@ -8,18 +8,27 @@ from kivy.core.window import Window
 
 class WaitBar(Widget):
 
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
         self.pos = (10, 10)
-        self.animation = Animation(pos=((Winwow.width * 0.9) - 64, 10), t='in_out_cubic') + Animation(pos=(10, 10), t='in_out_cubic')
-        self.animation.repeat = True
-        self.animation.start(self)
+        self.set_animation(Window.width)
         Window.bind(on_resize=self.on_window_resize)
 
-    def on_window_resize(self, window, width, height):
-        self.animation.cancel(self)
-        self.animation = Animation(pos=((width * 0.9) - 64 , 10), t='in_out_cubic') + Animation(pos=(10, 10), t='in_out_cubic')
+    def set_animation(self, width: int) -> None:
+        """The end abscissa is defined by the current window width by 0.9, the
+        popup abscissa size_hint, substract by 64, the size of the rectangle
+        (30) plus the popup border (24) plus the end offset we want (10).
+        """
+        self.animation = Animation(pos=((width * 0.9) - 64, 10), t='in_out_cubic')
+        self.animation += Animation(pos=(10, 10), t='in_out_cubic')
         self.animation.repeat = True
+
+    def start_animation(self):
+        self.animation.start(self)
+
+    def on_window_resize(self, window: Window, width: int, height: int) -> None:
+        self.animation.cancel(self)
+        self.set_animation(width)
         self.animation.start(self)
 
 class RootWidget(AnchorLayout):
