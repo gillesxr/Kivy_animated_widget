1c4993910ad0821200a1b12994aaa9bebd2b4970
gillesxr <gillesxr@tutanota.com>
2024-10-28 09:13:59 +0100

    Creation of the WaitBar widget animation

--- awesome_widget/app_with_kv.py
+++ awesome_widget/app_with_kv.py
@@ -1,7 +1,18 @@
 import kivy
 kivy.require('2.3.0')
+from kivy.animation import Animation
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.widget import Widget
+
+class WaitBar(Widget):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.pos = (10, 10)
+        animation = Animation(pos=(656, 10), t='in_out_cubic') + Animation(pos=(10, 10), t='in_out_cubic')
+        animation.repeat = True
+        animation.start(self)
 
 class RootWidget(AnchorLayout):
     pass
